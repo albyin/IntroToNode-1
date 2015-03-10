d3f8605fac1a9db1223cdcb4ce6d6147fb141ccf
SAM HAINS <sdh@eml.cc>
2015-03-10 17:34:28 -0400

    finished first few questions

--- async.js
+++ async.js
@@ -1,12 +1,27 @@
-var nodeStartingFunctionFileCode = require("./nodeStartingFunction");
-nodeStartingFunctionFileCode.nodeStartingFunction();
 var fs = require('fs');
-fs.readFile('./file1.txt', function(err, data) {
-    console.log(data);  // buffer object
-    console.log(data.toString());
+var async = require('async');
+var logFileArray = [];
+
+
+var doneFuncMaker = function(argv){
+	return function(done){
+		fs.readFile('./'+argv, function(err,data){
+			done(null,data);
+		});
+	};
+};
+
+
+for(var i=2;i<process.argv.length;i++){
+	logFileArray.push(doneFuncMaker(process.argv[i]));
+}
+
+async.parallel(logFileArray, function(err,data){
+	if(err)
+		return console.error(err);
+	else {
+		var arr = data.toString().split(',');
+		arr.forEach(function(e){console.log(e);});
+	}
 });
-
-
-
-
 
