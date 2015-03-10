a7eee4c47f7a5e885853b05a449300561e1726a3
SAM HAINS <sdh@eml.cc>
2015-03-10 19:32:14 -0400

    finished everything but with weird text formatting issue on final q

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ var fs = require('fs');
 var express = require('express');
 var logger = require('morgan');
 var async = require('async');
+var ejs = require('ejs');
+var socketio = require('socket.io');
+// create the express app
+var app = express();
+var server = app.listen(1234);
+var io = socketio.listen(server);
 var logFileArray = [];
+
+app.set('view engine', 'ejs');
+
+// connect the Morgan logging middleware to our app
+app.use( logger('dev') );
+
+
 
 var doneFuncMaker = function(argv){
 	return function(done){
@@ -18,33 +31,53 @@ var doneFuncMaker = function(argv){
 // now we can watch three files using:
 // node app.js file1.js file2.js file3.js
 var filenames = Array.prototype.slice.call(process.argv, 2);
-console.log(filenames);
+//console.log(filenames);
 
 filenames.forEach(function(file){
 	logFileArray.push(doneFuncMaker(file));
 });
 
-// create the express app
-var app = express();
 
-// connect the Morgan logging middleware to our app
-app.use( logger('dev') );
 
-// start a server listening on port 1234
-app.listen( 1234 );
+
+app.get('/', function (request, response) {
+  var mapFilenamesToContent = function(filename, doneCallback) {
+  	fs.readFile('./'+filename,function(err,data){
+  		//console.log(filename,filename.replace(/[^0-9]/ig));
+  		return doneCallback(null,{ id: filename.replace(/[^0-9]/ig, ""),data: data.toString(),filename: filename});
+  	});
+    // your code here, and in parameter list above
+    //return doneCallback(null, contentString ));
+  };
+
+  async.map(filenames, mapFilenamesToContent, function (err, results) {
+    if (err) console.log('async.map error:', err);
+    response.render( 'mainView',{files: results} );
+  });
+
+
+
+ //  async.parallel(logFileArray, function(err,data){
+	// 	if(err)
+	// 		return console.error(err);
+	// 	else {
+	// 		response.send('<pre>' + data.toString() + '</pre>');
+	// 	}
+	// });
+});
+
+fs.watch('./',function(event,filename){
+	fs.readFile('./'+filename,function(err,data){
+		if(err) return console.error(err);
+		console.log("filechanged");
+  		io.sockets.emit('filechanged', { filename: filename, data:data.toString(),id: filename.replace(/[^0-9]/ig, "")});
+  		
+  	});
+
+});
+
 
 // when someone requests http://localhost:1234/, run the callback
 // function listed here and respond with the data
 // we call this the "/" (or "Root") route.
-app.get("/", function(request, response) {
-  // fs.readFile(filenames[0], function(err, data) {
-  //   response.send('<pre>' + data.toString() + '</pre>');
-  // });
-	async.parallel(logFileArray, function(err,data){
-		if(err)
-			return console.error(err);
-		else {
-			response.send('<pre>' + data.toString() + '</pre>');
-		}
-	});
-});
+
